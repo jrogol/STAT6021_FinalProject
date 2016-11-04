cd180a75b60156f7cc33a522e0bf17aa46c1d144
jrogol <rogol@virginia.edu>
2016-11-04 16:28:21 -0400

    Final Scraper
    Updated to read in currency list.

--- MonthlyCurrencyAverage.py
+++ MonthlyCurrencyAverage.py
@@ -4,20 +4,23 @@ import time
 import re
 from bs4 import BeautifulSoup
 import urllib.request
+import os
 
+os.chdir('/Users/jamesrogol/Box Sync/Fall 2016/STAT 6021/Final Project/STAT6021_FinalProject')
+
+# Read in the Currency values
+data = pd.read_csv('Currencies.csv', header=None, delimiter=',,')
+
+# The abbrviations are all we care about - remove NaN values and duplicates.
+currencies = data[1].dropna().unique()
 years = range(2006,2017)
-currencies = {'Belgium':'EUR','Russia':'RUB'}
-
-
-# html = urllib.request.urlopen("http://www.x-rates.com/average/?from=USD&to=GBP&amount=1&year=2007")
-# soup = BeautifulSoup(html,"html5lib")
 
 AllData = pd.DataFrame(columns=['currency','rate','month','year','days'])
 
 for j in currencies:
     for year in years:
         try:
-            url = 'http://www.x-rates.com/average/?from=%s&to=GBP&amount=1&year=%s' % (currencies[j], year)
+            url = 'http://www.x-rates.com/average/?from=%s&to=GBP&amount=1&year=%s' % (j, year)
             html = urllib.request.urlopen(url)
             soup = BeautifulSoup(html,"html5lib")
             rates = soup.find('ul', attrs={'class':'OutputLinksAvg'}).findChildren('span',
@@ -30,7 +33,7 @@ for j in currencies:
                 rate = rates[i].contents
                 month = months[i].contents
                 day = re.sub('[^0-9]+','',days[i].contents[0])
-                temp = pd.DataFrame({'currency':currencies[j],
+                temp = pd.DataFrame({'currency':j,
                                     'rate':rate,
                                     'month':month,
                                     'year':year,
@@ -38,8 +41,8 @@ for j in currencies:
                 AllData = pd.concat([AllData, temp], axis=0)
             time.sleep(10+np.random.uniform(-5,5))
         except:
-            print('Error in %s, %s' % (currencies[j], year))
-        print(currencies[j],year)
+            print('Error in %s, %s' % (j, year))
+        print(j,year)
         
     time.sleep(10+np.random.uniform(-10,10))
     
